65e1f796d9b418211b958a6626dc136bea6911ca
Macks3110 <55533371+Macks3110@users.noreply.github.com>
2022-10-05 09:26:30 +0200

    Delete src directory

--- src/utils/misc.py
+++
@@ -1,9 +0,0 @@
-from functools import reduce
-
-
-def flatten(listOfLists):
-    return reduce(list.__add__, listOfLists, [])
-
-
-def without(take_them, but_not_these):
-    return [i for i in take_them if i not in but_not_these]
